49f6787996c94d486cdab75348cf1a1f88463747
student <student@stud.KHPK.RU>
2022-04-27 12:08:55 +0700

    first commit

---
+++ PR5/AddEditPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PR5
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Window
+    {
+        
+        private @class _currentClass = new @class();
+        public AddEditPage(@class selectedClass)
+        {
+            InitializeComponent();
+            if (selectedClass != null)
+                _currentClass = selectedClass;
+
+            
+            DataContext = _currentClass;
+            
+
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentClass.name_class.ToString()))
+                errors.AppendLine("Укажите ID");
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            {
+                PR5_BaseEntities.Get_context().@class.Add(_currentClass);
+
+
+                try
+                {
+                    PR5_BaseEntities.Get_context().SaveChanges();
+                    MessageBox.Show("Информация сохранена");
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+
+            }
+        }
+
+    }
+}
---
+++ PR5/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace PR5
+{
+    public class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DGridClass.ItemsSource = PR5_BaseEntities.Get_context().@class.ToList();
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddEditPage addEditPage = new AddEditPage(null);
+            addEditPage.Show();
+            this.Close();
+            //Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            if (DGridClass.SelectedItems.Count > 0)
+            {
+                for (int i = 0; i < DGridClass.SelectedItems.Count; i++)
+                {
+                    @class phone = DGridClass.SelectedItems[i] as @class;
+                    if (phone != null)
+                    {
+                        PR5_BaseEntities.Get_context().@class.Remove(phone);
+                    }
+                }
+            }
+            PR5_BaseEntities.Get_context().SaveChanges();
+            PR5_BaseEntities.Get_context().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+            DGridClass.ItemsSource = PR5_BaseEntities.Get_context().@class.ToList();
+        }
+
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                PR5_BaseEntities.Get_context().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridClass.ItemsSource = PR5_BaseEntities.Get_context().@class.ToList();
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            AddEditPage addEditPage = new AddEditPage((sender as Button).DataContext as @class);
+            addEditPage.Show();
+            this.Close();
+        }
+    }
+}
